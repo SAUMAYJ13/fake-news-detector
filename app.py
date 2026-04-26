ee29e2b03f5a5886af03c1e01aba09b08cbf40e9
SAUMAYJ13 <saumaysjadhav.2005@gmail.com>
2026-04-26 17:52:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,12 +23,12 @@ def train_model():
     ##real = pd.read_csv("True.csv")
    import pandas as pd
 
-url_fake = "https://storage.googleapis.com/dataset-uploader/fake.csv"
-url_real = "https://storage.googleapis.com/dataset-uploader/true.csv"
+    url_fake = "https://storage.googleapis.com/dataset-uploader/fake.csv"
+    url_real = "https://storage.googleapis.com/dataset-uploader/true.csv"
 
-fake = pd.read_csv(url_fake)
-real = pd.read_csv(url_real)
-
+    fake = pd.read_csv(url_fake)
+    real = pd.read_csv(url_real)
+ 
     fake["label"] = 0
     real["label"] = 1
 
